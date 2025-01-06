a40661cc828db51dfda46dd937bd1e24a18bf5d5
nichakorn-bi <nichakorn.bi@gmail.com>
2025-01-06 23:03:19 +1100

    Light and Dark Mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,30 @@
-import Link from "next/link";
-
-import { Button, buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/paths";
-
 import { LucideKanban } from "lucide-react";
+import Link from "next/link";
+import { homePath, ticketsPath } from "@/paths";
+import { ThemeSwitcher } from "./theme/theme-switcher";
+import { buttonVariants } from "./ui/button";
 
-export const Header = () => {
+const Header = () => {
   return (
     <nav
-      className="supports-backdrop-blur:bg-background/60
-          fixed left-0 right-0 top-0 z-20
-          border-b bg-background/95 backdrop-blur
-          w-full flex py-2.5 px-5 justify-between"
+      className="
+        supports-backdrop-blur:bg-background/60
+        fixed left-0 right-0 top-0 z-20
+        border-b bg-background/95 backdrop-blur
+        w-full flex py-2.5 px-5 justify-between
+      "
     >
-      <div>
-        <Button asChild variant="ghost">
-          <Link href={homePath()} className="text-lg font bold">
-            <LucideKanban />
-            <h1 className="text-lg font-semibold">TicketBounty</h1>
-          </Link>
-        </Button>
+      <div className="flex align-items gap-x-2">
+        <Link
+          href={homePath()}
+          className={buttonVariants({ variant: "ghost" })}
+        >
+          <LucideKanban />
+          <h1 className="text-lg font-semibold">TicketBounty</h1>
+        </Link>
       </div>
-      <div>
+      <div className="flex align-items gap-x-2">
+        <ThemeSwitcher />
         <Link
           href={ticketsPath()}
           className={buttonVariants({ variant: "default" })}
@@ -32,3 +35,5 @@ export const Header = () => {
     </nav>
   );
 };
+
+export { Header };
