11bffc2854b5b989a5d564b564d9f65d65fb3f62
nichakorn-bi <nichakorn.bi@gmail.com>
2025-01-07 14:23:02 +1100

    finish server components

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import { Placeholder } from "@/components/placeholder";
 import { Button } from "@/components/ui/button";
-//import { initialTickets } from "@/data";
+import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { ticketsPath } from "@/paths";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,33 +1,16 @@
-//"use client";
-
 import { Heading } from "@/components/heading";
-import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
-// import { Ticket } from "@/features/ticket/types";
-// import { useState, useEffect } from "react";
+import { TicketList } from "@/features/ticket/components/ticket-list";
+import { Suspense } from "react";
+import { Spinner } from "@/components/spinner";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-  // const [tickets, setTickets] = useState<Ticket[]>([]);
-
-  // useEffect(() => {
-  //   const fetchTickets = async () => {
-  //     const result = await getTickets();
-  //     setTickets(result);
-  //   };
-
-  //   fetchTickets();
-  // },[]);
+const TicketsPage = () => {
 
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
-
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense fallback={<Spinner/>}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
